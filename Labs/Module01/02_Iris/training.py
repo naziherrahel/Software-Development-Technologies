37967b66ff2be44e74f939029ac49d79a1b2dae7
drakeDjkw <sandrakjikwa92@gmail.com>
2025-02-13 00:24:40 +0800

    I changes some code on the file training.py

--- Labs/Module01/02_Iris/training.py
+++ Labs/Module01/02_Iris/training.py
@@ -1,4 +1,4 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
