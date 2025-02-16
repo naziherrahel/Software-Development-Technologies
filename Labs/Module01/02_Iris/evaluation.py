e8dc33ba59cbe53ebb7ab3d87d39f7bbb540ce10
drakeDjkw <sandrakjikwa92@gmail.com>
2025-02-16 20:27:48 +0800

    added data-evaluation script

--- Labs/Module01/02_Iris/evaluation.py
+++ Labs/Module01/02_Iris/evaluation.py
@@ -2,18 +2,17 @@ from sklearn.metrics import classification_report
 from preprocessing import load_data, preprocess_data
 from training import train_model
 
-# Evaluate the model with more details
+#evalution the model with more details
 def evaluate_model(data, model):
-    X = data.drop("target", axis=1)
-    y = data["target"]
+  x = data.drop("target",axis=1)
+  y = data["target"]
 
-    predictions = model.predict(X)
-    report = classification_report(y, predictions)
-    print("Classification Report:")
-    print(report)
+  predictions = model.predict(x)
+  report = classification_report(y, predictions)
+  print(report)
 
-if __name__ == "__main__":
-    data = load_data()
-    data = preprocess_data(data)
-    model = train_model(data)
-    evaluate_model(data, model)
+if _name_ == "_main_":
+  data = load_data()
+  data = preprocess_data(data)
+  model = train_model(data)
+  evaluate_model(data, model)
